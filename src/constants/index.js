7cf4f932ae005b5a6a047a25fca382c324319188
Saidul <sh0034879@gmail.com>
2024-05-22 00:42:16 +0530

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import pr1 from "../assets/projects/pr-1.png";
+import pr2 from "../assets/projects/pr-2.jpg";
+import pr3 from "../assets/projects/pr-3.jpg";
+import pr4 from "../assets/projects/pr-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -40,32 +40,32 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    image: project1,
+    title: "Jobify Application",
+    image: pr1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functional Job Create website with features like Job create, Job add $ remove, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Express.js", "MongoDB", "JWT"],
   },
   {
-    title: "Task Management App",
-    image: project2,
+    title: "Todo App",
+    image: pr2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
     title: "Portfolio Website",
-    image: project3,
+    image: pr3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
-    image: project4,
+    title: "CV creation Application",
+    image: pr4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    technologies: ["HTML", "CSS", "Next.js", "Express", "MongoDB"],
   },
 ];
 
